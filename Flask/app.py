c7632a48950cd8e87130649b142c89ac0efef2cf
Vincent Yuan <vincentyuan1020@gmail.com>
2025-10-02 19:26:34 -0400

    add d

--- Flask/app.py
+++ Flask/app.py
@@ -1,1 +1,11 @@
-import flask from Flask
+from Flask import Flask
+
+app = Flask(__name__) 
+
+app.route('/')
+def index():
+	return 'Hello, Flask!'
+
+if __name__ == '__main__':
+	app.run(debug=True)
+	print("Hello World")
