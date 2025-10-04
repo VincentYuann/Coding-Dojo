8f82debe02806a42c0ee95edaf0548c02780e2f6
Vincent Yuan <vincentyuan1020@gmail.com>
2025-10-04 11:00:41 -0400

    Learn basic html templates

--- Flask/app.py
+++ Flask/app.py
@@ -3,6 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 # My app
 app = Flask(__name__) 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class MyModel(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(80), unique=True, nullable=False)
+
+	def __repr__(self):
+		return f'<MyModel {self.name}>'
 
 # Home page route
 @app.route('/')
